b0e9bd4333927bd97a0abdfaca6efdd6fc8ba431
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-19 10:27:10 +0530

    Add files via upload

---
+++ inheritance/inheritance_2.java
@@ -0,0 +1,30 @@
+// CODE:-
+
+package CODE.INHERITANCE;
+
+class Vehicle
+{
+    void drive()
+    {
+        System.out.println("Driving a car");
+    }
+}
+
+class Car extends Vehicle
+{
+    void drive()
+    {
+        System.out.println("Repairing a car");
+    }
+}
+
+class Main{
+    public static void main(String st[])
+    {
+        Vehicle obj1 = new Vehicle();
+        Car obj2 = new Car();
+
+        obj1.drive();
+        obj2.drive();
+    }
+}
