40510afe01fff2fd357d8529d8e66a3cbb98308c
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-19 10:33:44 +0530

    inheritance_2.java

--- inheritance/inheritance_2.java
+++ inheritance/inheritance_2.java
@@ -1,4 +1,4 @@
-// CODE:-
+// 2. Write a Java program to create a class called Vehicle with a method called drive(). Create a subclass called Car that overrides the drive() method to print "Repairing a car".
 
 class Vehicle
 {
@@ -27,3 +27,4 @@ class Main{
     }
 
 }
+
