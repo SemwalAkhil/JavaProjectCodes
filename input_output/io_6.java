65de2398f4780720f2a9c4d0ddaf103c3513b7c0
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-21 12:14:08 +0530

    Update io_6.java

--- input_output/io_6.java
+++ input_output/io_6.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // Create Scanner object to read input from console
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a boolean value (true/false): ");
         boolean flag = sc.nextBoolean(); // read boolean input
 
         System.out.println("You entered: " + flag);
-
-        // Close the scanner
-        sc.close();
     }
 }
