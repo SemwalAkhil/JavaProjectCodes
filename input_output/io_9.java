a2b9235c1eec3913cf0cf44732bb862598028280
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-21 12:15:31 +0530

    Update io_9.java

--- input_output/io_9.java
+++ input_output/io_9.java
@@ -5,7 +5,6 @@ import java.io.*;
 class Main {
     public static void main(String[] args) {
         try {
-            // Create FileReader object to read from file
             FileReader fr = new FileReader("input.txt"); // make sure input.txt exists
 
             int ch;
@@ -14,9 +13,6 @@ class Main {
             while ((ch = fr.read()) != -1) {
                 System.out.print((char) ch);
             }
-
-            // Close the FileReader
-            fr.close();
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e);
