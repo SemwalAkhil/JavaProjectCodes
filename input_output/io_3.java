efd50bcbd97dcbdd8faa730cb6b25d0ebac453ed
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-21 12:13:03 +0530

    io_3.java

--- input_output/io_3.java
+++ input_output/io_3.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // Create Scanner object to read input from console
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter an integer: ");
         int num = sc.nextInt(); // read integer input
 
         System.out.println("You entered: " + num);
-
-        // Close the scanner
-        sc.close();
     }
 }
