220ba9ec2a230cef75e50d78d4d6ee7ddf526732
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-21 12:14:48 +0530

    Update io_7.java

--- input_output/io_7.java
+++ input_output/io_7.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // Create Scanner object to read input from console
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a short integer: ");
         short num = sc.nextShort(); // read short input
 
         System.out.println("You entered: " + num);
-
-        // Close the scanner
-        sc.close();
     }
 }
