fd053647283a96f96eaaaa967fade5345409f0e5
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-21 12:35:54 +0530

    Update jdbc_4.java

--- java_database_connectivity/jdbc_4.java
+++ java_database_connectivity/jdbc_4.java
@@ -6,7 +6,7 @@ class Main{
     public static void main(String s[]){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bca2ndyear_4090", "root", "@Gagan123");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bca2ndyear_4090", "root", "root");
             Statement st = c.createStatement();
             int x = st.executeUpdate("UPDATE java SET Rollno = 90 where Rollno = 4090");
 
@@ -22,4 +22,5 @@ class Main{
             System.out.println(e);
         }
     }
+
 }
