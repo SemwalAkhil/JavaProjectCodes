27d8ff298337bf78178104445181b8446d7ead4d
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-21 12:15:04 +0530

    Update io_8.java

--- input_output/io_8.java
+++ input_output/io_8.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // Create Scanner object to read input from console
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a long integer: ");
         long num = sc.nextLong(); // read long input
 
         System.out.println("You entered: " + num);
-
-        // Close the scanner
-        sc.close();
     }
 }
