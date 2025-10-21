2865db1d1a614eb7260379f946fedbb2c76f9dbe
Gagandeep Singh <gagansaini7998@gmail.com>
2025-10-21 12:13:50 +0530

    io_5.java

--- input_output/io_5.java
+++ input_output/io_5.java
@@ -3,16 +3,12 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
-        // Create Scanner object to read input from console
+    public static void main(String[] args) {   
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a double value: ");
         double num = sc.nextDouble(); // read double input
 
         System.out.println("You entered: " + num);
-
-        // Close the scanner
-        sc.close();
     }
 }
